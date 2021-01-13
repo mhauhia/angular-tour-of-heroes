a84352e23dafae1319c29d658c39f37ce61d07f1
Markus Hauhia <s2000579@edu.bc.fi>
2021-01-13 15:14:38 +0200

    Tilanne osa 2. Display-a-list  jälkeen (https://angular.io/tutorial/toh-pt2)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,13 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
 
   hero: Hero = {
     id: 1,
